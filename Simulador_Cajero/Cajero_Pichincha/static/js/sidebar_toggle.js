9d46cfc34f007f17fc3194652aab7fca4327ac2a
SaidNa1402 <151399832+SaidNa1402@users.noreply.github.com>
2025-01-30 13:10:35 -0500

    Delete Simulador_Cajero directory

--- Simulador_Cajero/Cajero_Pichincha/static/js/sidebar_toggle.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById("menu-toggle").addEventListener("click", function(e) {
-        e.preventDefault();
-        document.getElementById("wrapper").classList.toggle("toggled");
-    });
-});
